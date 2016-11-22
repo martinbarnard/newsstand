8de4862dec3712ae3d335fc26f57339d48ca303f
Martin Barnard <barnard.martin@gmail.com>
2016-11-22 20:31:47 +0000

    feature polished: todo: threading

--- index.py
+++ index.py
@@ -10,28 +10,131 @@
 
 # Load libraries
 import requests
-import urllib
-import os
-import json
-import yaml
+import urllib, os, json, yaml
 from flask import Flask, request
+# 21co wallet 
+print('importing two1 stuff')
 from two1.wallet import Wallet
 from two1.bitserv.flask import Payment
+from random import choice
+import argparse
+# Using mongodb
+from pymongo import MongoClient 
+# for generating unique ids
+from hashlib import sha256
+ids=lambda st : sha256(st.encode('utf-8'))
 
-# Init Flask, Wallet and Payment
+
+# TODO: threading to periodically pull all sources...
+# TODO: use 10' timer to pull all sources and then provide an aggregate here...
+#tsecs=10*60
+
+
+def init_db():
+    client = MongoClient()
+    db = client.newsstand
+    return db.articles
+
+db = init_db()
+print("db initialised")
+
+key='f26c6e07aac5401eb95f71a9c4f70db1'
 app = Flask(__name__)
+parser = argparse.ArgumentParser()
+parser.add_argument('-p','--port', help="which port to start on", type=int, default=10254)
+args=parser.parse_args()
+# Init Flask, Wallet and Payment
 wallet = Wallet()
 payment = Payment(app, wallet)
+# TODO: Store all articles received in db
+# Don't like these hard-coded. Maybe store in config later?
+categories = [ 
+        'business', 'entertainment', 
+        'gaming', 'general', 'music', 
+        'science-and-nature', 'sport', 'technology' 
+        ]
+languages = ['en', 'de', 'fr']
+countries = [
+        'au', 'de', 'gb', 'in', 'it', 'us'
+        ]
 
-# Add 402
+newsapi='https://newsapi.org/v1/'
+# our list of sources
+sourceList=[]
+
+def get_sources():
+    sl=[]
+    url=requests.get(newsapi + 'sources')
+    p=json.loads(url.text)
+    for s in p['sources']:
+        sl.append(s)
+    return sl
+
+@app.route('/sources')
+def sources():
+    rval=[]
+    for s in sourceList:
+        rval.append(s['id'])
+    print('yeah')
+    return json.dumps(rval)
+
 @app.route('/news')
-@payment.required(900)
-def lookup_string():
-    source = request.args.get('source')
-    key = os.environ.get('KEY')
-    url = requests.get('https://newsapi.org/v1/articles?source='+source+'&sortBy=latest&apiKey='+key)
-    return url.text
+#@payment.required(900)
+def get_articles():
+    source=request.args.get('source') 
+    ttls=request.args.get('titles')
+    if source == None:
+        source=choice(sourceList)
+        srcstring = 'source={}'.format(source['id'])
+        srtstring = '&sortBy={}'.format(source['sortBysAvailable'][0])
+    else:
+        print('using {}'.format(source))
+        srt='latest'
+        for k in sourceList:
+            if k['id'] == source:
+                srt=k['sortBysAvailable'][0]
+        srtstring='&sortBy={}'.format(srt)
+        # TODO: check source exists and also sort is valid
+        srcstring='source={}'.format(source)
+
+
+    urlstring=newsapi + 'articles?' + srcstring + srtstring + '&apiKey=' + key #+ country + language
+    print("calling: {}".format(urlstring))
+    url = requests.get(urlstring)
+    titles=[]
+    articles=[]
+    bl=json.loads(url.text)
+    xx=add_records(db,bl)
+    if ttls != None and ttls.upper()=='Y':
+        for l in bl['articles']:
+            titles.append(l['title'])
+        return json.dumps(titles)
+    return json.dumps(bl['articles'])
+
+
+
+def add_records(coll,recs):
+    rvs=[]
+    if 'articles'  in recs:
+        for r in recs['articles']:
+            x=ids(r['url'])
+            xd =x.hexdigest()
+            r['_id']=xd
+            if not coll.find_one({'_id':xd}):
+                print("adding... {}".format(r['title']))
+                coll.insert_one(r)
+            else:
+                print('{} already there'.format(xd))
+    return 0
+#    ids= coll.insert_many(recs)
+#    return ids
+
 
 # Init Host
 if __name__=='__main__':
-    app.run(host='::', port='10125')
+    print("initalising...")
+    port=args.port
+    sourceList=get_sources()
+    print('sources acquired:')
+
+    app.run(host='::', port=port)
