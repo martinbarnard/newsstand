3d0152bfea3158f54f6cec02f7d73438d5100d79
Martin Barnard <barnard.martin@gmail.com>
2016-12-04 10:33:58 +0000

    separating out the news gatherer

---
+++ config.py
@@ -0,0 +1,17 @@
+# Config file for newsstand 2.0
+#
+#[networking]
+port= 20055
+# [misc]
+sleep_minutes= 20
+newsapi= 'https://newsapi.org/v1/'
+categories = [ 
+    'business', 'entertainment', 
+    'gaming', 'general', 'music', 
+    'science-and-nature', 'sport', 'technology' 
+    ]
+languages=['en', 'de', 'fr'],
+countries= [ 'au', 'de', 'gb', 'in', 'it', 'us' ],
+
+#[security]
+key= 'f26c6e07aac5401eb95f71a9c4f70db1'
--- index.py
+++ index.py
@@ -8,60 +8,56 @@
 # GitHub Repository: Newsstand (github.com/cryptopelago/newsstand)
 # License: Unlicense (unlicense.org)
 
+from config import *
+
 # Load libraries
 import requests
 import urllib, os, json, yaml
-from flask import Flask, request, jsonify
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+             render_template, flash 
 import dateutil.parser
 import datetime
 
+import os
 # 21co wallet 
 print('importing two1 stuff')
 from two1.wallet import Wallet
 from two1.bitserv.flask import Payment
+# 
+# 
 from random import choice
-import argparse
+
+#
 # Using mongodb
 from pymongo import MongoClient 
+print("pymongo started")
+#
 # for generating unique ids
 from hashlib import sha256
 ids=lambda st : sha256(st.encode('utf-8'))
 
 
 import threading, time
-# TODO: threading to periodically pull all sources...
-# TODO: use 10' timer to pull all sources and then provide an aggregate here...
-tsecs=60*60
+sleepytime= lambda  mins:  mins * 60
 threads=[]
 
 def init_db():
     client = MongoClient()
-    db = client.newsstand
-    return db.articles
+    cl = client.newsstand
+    print('yeah baby')
+    return cl.articles
 
 db = init_db()
 
-key='f26c6e07aac5401eb95f71a9c4f70db1'
 app = Flask(__name__)
-parser = argparse.ArgumentParser()
-parser.add_argument('-p','--port', help="which port to start on", type=int, default=10254)
-args=parser.parse_args()
+
+# 21.co wallet & payment
 # Init Flask, Wallet and Payment
+print("Initiating wallet stuff")
 wallet = Wallet()
 payment = Payment(app, wallet)
-# TODO: Store all articles received in db
-# Don't like these hard-coded. Maybe store in config later?
-categories = [ 
-        'business', 'entertainment', 
-        'gaming', 'general', 'music', 
-        'science-and-nature', 'sport', 'technology' 
-        ]
-languages = ['en', 'de', 'fr']
-countries = [
-        'au', 'de', 'gb', 'in', 'it', 'us'
-        ]
+print("Wallet initiated")
 
-newsapi='https://newsapi.org/v1/'
 # our list of sources
 sourceList=[]
 
@@ -79,22 +75,42 @@ def sources():
         rval.append(s['id'])
     return json.dumps(rval)
 
+@app.route('/')
+def index():
+    nw=datetime.datetime.now()
+    hrs=datetime.timedelta(hours=1)
+    after=nw - hrs
+    print("Getting rows...")
+    rows=db.find({'date':{'$gt':after}})
+    print("Rows gotten")
+    return render_template('index.html', rows=rows)
+
 @app.route('/news')
 #@payment.required(900)
 def get_articles():
     '''
     pulls from our database which is set to 
     update every n-minutes
+    args are:
+    source 
+    categories 
+    before - time: %Y-%m-%d %H:%M:%S
+    after - time: %Y-%m-%d %H:%M:%S
     '''
-    source=request.args.get('source','all')
-    categories=request.args.get('articles', 'all')
+    # TODO:  Utilise these
+    source      = request.args.get('source','all')
+    categories  = request.args.get('categories', 'all')
+    before      = request.args.get('before', '')
+    after       = request.args.get('after', '')
+    author      = request.args.get('author', '')
 
     # fields here...
-    tora=datetime.datetime.now()
-    hrs=datetime.timedelta(hours=6)
-    after=tora - hrs
-    rv=[]
+    nw=datetime.datetime.now()
+    hrs=datetime.timedelta(hours=1)
+    after=nw - hrs
+    rv=['{}'.format(source)]
     rows=db.find({'date':{'$gt':after}})
+#    rows=db.find()
     if rows:
         for row in rows:
             k={}
@@ -105,7 +121,7 @@ def get_articles():
                 else:
                     k[r]=row[r].strftime('%Y-%m-%d %H:%M:%S')
             rv.append(k)
-    return json.dumps(rv)
+    return json.dumps(rv[1:])
 
 
 
@@ -121,7 +137,7 @@ def get_url(urlstring, ttls=None):
     url = requests.get(urlstring)
     titles=[]
     bl=json.loads(url.text)
-    xx=add_records(db,bl)
+    #xx=add_records(db,bl)
     return bl['articles']
 
 def get_sources():
@@ -135,7 +151,7 @@ def get_sources():
         sl.append(s)
     return sl
 
-def articles(src,categories=categories,key=key, db=db, tsecs=tsecs):
+def articles(src, db=db):
     '''
     update db with latest articles from src with cat
     will be threaded
@@ -143,28 +159,32 @@ def articles(src,categories=categories,key=key, db=db, tsecs=tsecs):
     src - string of source id
     returns - nothing
     '''
+    tts=sleepytime(sleep_minutes)
     while True:
+        print("awake & getting {}".format(src['id']))
         srcstring = 'source={}'.format(src['id'])
         for cat in categories:
             urlcat="&category={}".format(cat)
-            urlstring=newsapi + 'articles?' + srcstring + urlcat + '&apiKey=' + key 
+            urlstring=newsapi + 'articles?' + srcstring + urlcat + '&apiKey=' + key
             rvs=get_url(urlstring)
-            add_records(db, rvs)
-        time.sleep(tsecs)
+            add_records(db, rvs, cat, src['id'])
+        time.sleep(tts)
 
 
-def add_records(coll,recs):
+def add_records(coll,recs, cat, src):
     '''
     add records to coll 
     '''
     tora=datetime.datetime.now()
     rvs=[]
     # recs is a list
-    if 'articles'  in recs:
-        for r in recs['articles']:
+    if  recs:
+        for r in recs:
             x=ids(r['url'])
             xd =x.hexdigest()
             r['_id']=xd
+            r['src']=src
+            r['category']=cat
             datestring=r['publishedAt']
             if datestring:
                 try:
@@ -179,8 +199,8 @@ def add_records(coll,recs):
                 r['date']=tora
             if not coll.find_one({'_id':xd}):
                 try:
-                    print(">>> INSERTING {}".format(r['title']))
                     coll.insert_one(r)
+                    print(">>> {} {}".format(src,r['title']))
                 except OverflowError:
                     # we get here with the times of india having fucked up dates
                     print("we're in overflowzzz {}".format(r))
@@ -189,15 +209,20 @@ def add_records(coll,recs):
                         coll.insert_one(r)
                     except:
                         print("Fuck it's all died")
+    else:
+        print("No articles in {}".format(recs))
     return 0
+
 
 
 # Init Host
 if __name__=='__main__':
-    port=args.port
+    # TODO: Remove the  threaded daemon and put it in it's own file
+    # TODO: Wrap this in a loop &/or generate a config
+    # file if none exists
     sourceList=get_sources()
     for s in sourceList:
-        t = threading.Thread(target=articles, args=(s,))
+        t = threading.Thread(target=articles, args=(s, ))
         t.start()
         threads.append(t)
 
